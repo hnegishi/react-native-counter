ca39f42d3829acfa0d58e3ba722456e717d06b41
hnegishi <wxjyg60238@gmail.com>
2019-01-09 22:06:02 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import RootContainer from './containers/RootContainer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './redux/reducers';
+//import Counter from './components/Counter';
+
+const store = createStore(reducer);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <RootContainer />
+      </Provider>
+    );
+  }
+}
